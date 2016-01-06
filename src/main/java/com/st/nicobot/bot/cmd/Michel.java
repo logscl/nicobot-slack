6b3d3d40a7e355d366c377ca170d87c4d507b3db
Logs Cl <logs.be@gmail.com>
2016-01-06 22:56:23 +0100

    bisous michel

---
+++ src/main/java/com/st/nicobot/bot/cmd/Michel.java
@@ -0,0 +1,60 @@
+package com.st.nicobot.bot.cmd;
+
+import com.st.nicobot.bot.NicoBot;
+import com.st.nicobot.bot.utils.Option;
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Logs on 06-01-16.
+ */
+@Service
+public class Michel extends NiCommand {
+
+    private static final String COMMAND = "!michel";
+    private static final String FORMAT = "!michel";
+    private static final String DESC = "bisous michel ok";
+
+    private static List<String> FRAGMENTS = Arrays.asList("ok","michel","bisous","merci", "michel smits","appel moi","gsm");
+
+    @Autowired
+    private NicoBot nicobot;
+
+    @Override
+    public String getCommandName() {
+        return COMMAND;
+    }
+
+    @Override
+    public String getDescription() {
+        return DESC;
+    }
+
+    @Override
+    public String getFormat() {
+        return FORMAT;
+    }
+
+    @Override
+    protected void doCommand(String command, String[] args, Option opts) {
+        int wordCount = RandomUtils.nextInt(2,6);
+
+        StringBuilder phrase = new StringBuilder();
+
+        List<String> possibleWords = new ArrayList<>(FRAGMENTS);
+
+        for(int i = 0;i<wordCount;i++) {
+            int index = RandomUtils.nextInt(0, possibleWords.size());
+            String word = possibleWords.get(index);
+            phrase.append(word).append(" ");
+            possibleWords.remove(index);
+        }
+
+        nicobot.sendMessage(opts.message, phrase.toString());
+    }
+}
